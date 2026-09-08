dd7162d8773b60763242446825bef142a105d4ae
Omkar_Shirsekar <iamomkar0007@gmail.com>
2026-09-08 21:21:08 +0530

    fix:ws connection

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,10 @@
 import { prisma } from "db/client";
 
 export default async function Home() {
-  const users = await prisma.user.findMany();
+  // const users = await prisma.user.findMany();
   return (
     <div>
-      {JSON.stringify(users)}
+      {/* {JSON.stringify(users)} */}
     </div>
   );
 }
